3e479de322a0979a0bf9bb7e82eb171bec5d92c9
qcubed-4 <83004588+qcubed-4@users.noreply.github.com>
2021-04-23 18:22:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// The default is to redirect to the start page for development. Feel free to replace this index.php file with your own.
+header('Location: vendor/qcubed/application/tools/start_page.php');
+
