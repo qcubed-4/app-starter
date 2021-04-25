6ffe3a93b5858d65e8e17e9fe1f9783b82ffb3af
QCubed-4 <83004588+qcubed-4@users.noreply.github.com>
2021-04-25 18:17:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // The default is to redirect to the start page for development. Feel free to replace this index.php file with your own.
-header('Location: vendor/qcubed/application/tools/start_page.php');
+header('Location: vendor/qcubed-4/application/tools/start_page.php');
 
